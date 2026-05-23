d0c1c1cc5809a4a00d38bec5fb4d293b4787ead7
Abhi-221b <abhishekgupta.241081@gmail.com>
2026-05-24 01:29:42 +0530

    updating font import

--- src/main.tsx
+++ src/main.tsx
@@ -8,23 +8,31 @@ import Home from './pages/home/Home.tsx'
 import About from './pages/about/About.tsx'
 import Contact from './pages/contact/Contact.tsx'
 import Projects from './pages/projects/Projects.tsx'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/merriweather/400.css'
+import '@fontsource/merriweather/700.css'
+import '@fontsource/fira-code/400.css'
+import '@fontsource/fira-code/500.css'
+import '@fontsource/fira-code/700.css'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     Component: App,
     children: [
       { index: true, Component: Home },
-      { path: "about", Component: About },
-      { path: "contact", Component: Contact },
-      { path: "project", Component: Projects },
-    ],
-  },
+      { path: 'about', Component: About },
+      { path: 'contact', Component: Contact },
+      { path: 'project', Component: Projects }
+    ]
+  }
 ])
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
